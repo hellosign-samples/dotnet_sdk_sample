47a446f0a006f4a93ec3f2f9ef3637b4e1e038e1
Collin Wheeler <collinwheeler@hellosign.com>
2022-08-03 16:54:24 -0700

    Added model directory

---
+++ Sample_App/model/Signer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Sample_App.model
+{
+    public class Signer
+    {
+        private string emailAddress { get; }
+        private string name { get; }
+
+        public Signer(string name, string emailAddress)
+        {
+            this.name = name;
+            this.emailAddress = emailAddress;
+        }
+    }
+}
